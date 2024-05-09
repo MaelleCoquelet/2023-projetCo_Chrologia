5b5e473158df262331b1c2d368a6d3021dd82104
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-09 14:54:27 +0200

    ajout des icônes

---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M16 10.4905V21.481M21.5005 15.985H10.4995M23.0286 1H8.97143C4.07143 1 1 4.46813 1 9.37774V22.6223C1 27.5319 4.05714 31 8.97143 31H23.0286C27.9429 31 31 27.5319 31 22.6223V9.37774C31 4.46813 27.9429 1 23.0286 1Z"
+            stroke="#0F172A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
