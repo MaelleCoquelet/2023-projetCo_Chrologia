fe371ab231edb2f700f762b4f72e955c9977a0dc
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-09 15:29:03 +0200

    ajout de la structure de base html pour le composant footer page

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -4,5 +4,38 @@ import { RouterLink } from 'vue-router';
 </script>
 <template>
     <footer>
+        <ul>
+            <div>
+                <li>
+                    <RouterLink to="/">
+                        
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/">
+                        
+                    </RouterLink>
+                </li>
+            </div>
+            <div>
+                <li>
+                    <RouterLink to="/">
+                        
+                    </RouterLink>
+                </li>
+            </div>
+            <div>
+                <li>
+                    <RouterLink to="/">
+                        
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/">
+                        
+                    </RouterLink>
+                </li>
+            </div>
+        </ul>
     </footer>
 </template>
