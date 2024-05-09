80cf45a16d240be4483aacf54fef60ffbd8d04c7
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-09 15:39:04 +0200

    refactor: Remove unused CSS styles and add icons to FooterPage component

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 
 import { RouterLink } from 'vue-router';
+import IconBell from './icons/IconBell.vue';
+import IconLetter from './icons/IconLetter.vue';
+import IconPlus from './icons/IconPlus.vue';
+import IconSearch from './icons/IconSearch.vue';
+
 </script>
 <template>
     <footer>
@@ -8,31 +13,31 @@ import { RouterLink } from 'vue-router';
             <div>
                 <li>
                     <RouterLink to="/">
-                        
+                        <IconBell />
                     </RouterLink>
                 </li>
                 <li>
                     <RouterLink to="/">
-                        
-                    </RouterLink>
-                </li>
-            </div>
-            <div>
-                <li>
-                    <RouterLink to="/">
-                        
+                        <IconSearch />
                     </RouterLink>
                 </li>
             </div>
             <div>
                 <li>
                     <RouterLink to="/">
-                        
+                        <IconPlus />
+                    </RouterLink>
+                </li>
+            </div>
+            <div>
+                <li>
+                    <RouterLink to="/">
+                        <IconLetter />
                     </RouterLink>
                 </li>
                 <li>
                     <RouterLink to="/">
-                        
+                        <div></div>
                     </RouterLink>
                 </li>
             </div>
