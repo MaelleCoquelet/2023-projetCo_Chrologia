b2d3a3920363c74250bb6d1984fb3f00f4fbd617
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-15 09:22:51 +0200

    ajout d'une photo d'exemple, mise en forme du footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -8,39 +8,41 @@ import IconSearch from './icons/IconSearch.vue';
 
 </script>
 <template>
-    <footer class="relative" >
-        <ul class=" bg-slate-900 flex px-8 py-4 justify-between">
-            <div class="footer-items">
-                <li>
-                    <RouterLink to="/">
-                        <IconBell />
-                    </RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/">
-                        <IconSearch />
-                    </RouterLink>
-                </li>
-            </div>
-            <div class="bg-red-200 p-2 rounded-full absolute -top-1/3 translate-x-full">
-                <li>
-                    <RouterLink to="/">
-                        <IconPlus />
-                    </RouterLink>
-                </li>
-            </div>
-            <div class="footer-items">
-                <li>
-                    <RouterLink to="/">
-                        <IconLetter />
-                    </RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/">
-                        <div></div>
-                    </RouterLink>
-                </li>
-            </div>
-        </ul>
+    <footer class="relative">
+        <nav>
+            <ul class=" bg-slate-900 flex px-8 py-2 justify-between">
+                <div class="footer-items">
+                    <li>
+                        <RouterLink to="/">
+                            <IconBell />
+                        </RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/">
+                            <IconSearch />
+                        </RouterLink>
+                    </li>
+                </div>
+                <div class=" -translate-y-8 ">
+                    <li class="bg-red-200 p-2 rounded-full">
+                        <RouterLink to="/">
+                            <IconPlus />
+                        </RouterLink>
+                    </li>
+                </div>
+                <div class="footer-items">
+                    <li>
+                        <RouterLink to="/">
+                            <IconLetter />
+                        </RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/">
+                            <img class="rounded-full h-8" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+                        </RouterLink>
+                    </li>
+                </div>
+            </ul>
+        </nav>
     </footer>
 </template>
