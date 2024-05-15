335603fa6a876d330ae1e2c39a1c0b3a8da35b4e
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-15 14:21:51 +0200

    création de la structure en html du header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+
+import { RouterLink } from 'vue-router';
+import IconLogo from '@/components/icons/IconLogo.vue';
+import IconSettings from '@/components/icons/IconSettings.vue';
+</script>
+
 <template>
-  <header>AFAIRE</header>
+  <header class="fixed z-10 w-full top-0">
+    <nav>
+      <RouterLink to="/">
+        <IconLogo />
+      </RouterLink>
+      <RouterLink to="/">
+        <IconSettings />
+      </RouterLink>
+    </nav>
+  </header>
 </template>
