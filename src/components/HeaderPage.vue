ae0924ace974f7256aac1234ec874f1af260480f
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-15 16:30:17 +0200

    création des classes fixed-top, fixed-topNav et icon-fillActive

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -3,16 +3,23 @@
 import { RouterLink } from 'vue-router';
 import IconLogo from '@/components/icons/IconLogo.vue';
 import IconSettings from '@/components/icons/IconSettings.vue';
+import { ref, computed, watch } from 'vue'
+
+const settingActive = ref(false)
+
+function disableSetting() {
+  settingActive.value = false
+} 
 </script>
 
 <template>
-  <header class="fixed z-10 w-full top-0">
-    <nav>
-      <RouterLink to="/">
+  <header class="fixed-top py-6">
+    <nav class="flex justify-between px-[20px] items-center">
+      <RouterLink to="/" @click="disableSetting">
         <IconLogo />
       </RouterLink>
-      <RouterLink to="/">
-        <IconSettings />
+      <RouterLink to="/settings" @click="settingActive = !settingActive">
+        <IconSettings :class="{ 'icon-fillActive': settingActive }" />
       </RouterLink>
     </nav>
   </header>
