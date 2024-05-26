96fa5c248bd7d567368579bb33ae0751ed8df45b
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-26 17:09:54 +0200

    correction au niveau de la page reminder

---
+++ src/pages/reminder/[id].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import MenuFeedReminder from '@/components/MenuFeedReminder.vue'
+</script>
+<template>
+    <MenuFeedReminder />
+</template>
