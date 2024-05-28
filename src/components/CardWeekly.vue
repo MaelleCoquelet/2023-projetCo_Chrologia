d101c383db927e4e46cbd7915e7144f7f2403408
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-05-28 11:18:23 +0200

    mise ne place de la structure html du composant cardWeekly

---
+++ src/components/CardWeekly.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+import IconLike from '@/components/icons/IconLike.vue';
+import IconComment from '@/components/icons/IconComment.vue';
+import IconEmoji from '@/components/icons/IconEmoji.vue';
+import IconMore from '@/components/icons/IconMore.vue';
+
+const activeText = ref(false)
+</script>
+<template>
+    <article>
+        <div>
+            <RouterLink to="/">
+                <img class="rounded-full h-8" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+                <p>Nom Utilisateur</p>
+            </RouterLink>
+            <IconMore />
+        </div>
+        <div class="flex gap-1">
+            <p class="">Début de la description ici
+                <span class="text-orpink-200 contents" @click="activeText = !activeText"
+                    :class="{ '!hidden': activeText }">...voir plus</span>
+            </p>
+            <p class="hidden" :class="{ '!contents': activeText }">
+                fin de la description ici.
+            </p>
+        </div>
+        <div><img src="" alt=" Photo Ici">
+        </div>
+        <ul>
+            <li>
+                <IconLike />
+                <p>nb Like</p>
+            </li>
+            <li>
+                <IconComment />
+                <p>nb Comments</p>
+            </li>
+            <li>
+                <IconEmoji />
+            </li>
+        </ul>
+    </article>
+</template>
--- src/pages/weekly/index.vue
+++ src/pages/weekly/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 import MenuFeedWeekly from '@/components/MenuFeedWeekly.vue';
+import CardWeekly from '@/components/CardWeekly.vue';
 </script>
 <template>
- <MenuFeedWeekly />
+    <MenuFeedWeekly />
+    <main class="px-5">
+        <CardWeekly />
+    </main>
 </template>
