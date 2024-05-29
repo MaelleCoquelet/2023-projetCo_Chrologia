bc686077c3bd25a24569968f6908af37beee2b2e
AAmrouche <150058298+AAmrouche@users.noreply.github.com>
2024-05-29 14:31:01 +0200

    Button. vue + test sur app vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 import { RouterView } from 'vue-router'
 import HeaderPage from '@/components/HeaderPage.vue'
 import FooterPage from '@/components/FooterPage.vue'
+import Button from '@/components/Button.vue';
 </script>
 
+
 <template>
+  <button />
   <HeaderPage />
   <RouterView />
   <FooterPage />
---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+const props = withDefaults
+ (defineProps<{
+    variant?: 'default' | 'dark' 
+    size?:'small' | 'medium' 
+    text?: string
+    url?: string
+}>(),
+{
+    variant: 'default',
+    size: 'medium',
+}
+)
+
+const variantClass ={
+    default: 'bg-orpink-200 text-slate-900'
+    dark: ,    
+}
+
+
+</script>
