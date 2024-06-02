01d2ff8c7ff03f5483771885df1b4148cbf0b824
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-02 13:33:02 +0200

    création de la page profil, ajout de la structure html, ajout du lien dans le ficheir typed-router.d.ts

---
+++ src/pages/profil/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import CardWeekly from '@/components/CardWeekly.vue';
+</script>
+<template>
+    <main class="px-5 flex flex-col gap-12 py-12 mt-20">
+        <div>
+            <img src="/src/assets/img/palicoMHW.webp" alt="Bannière de profil">
+            <img class="rounded-full h-8" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+            <button>éditer le profil</button>
+            <div>
+                <div>
+                    <p>Amis<span>2</span></p>
+                </div>
+                <div>
+                    <p>Posts<span>2</span></p>
+                </div>
+                <div>
+                    <p>Likes<span>2</span></p>
+                </div>
+            </div>
+        </div>
+        <p>Reminder</p>
+        <p>Weekly</p>
+        <CardWeekly v-for="post in 5" />
+    </main>
+</template>
