d3a5cfc43153f8e872bf517fddcd6ae601750789
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-11 00:22:47 +0200

    ajout de la base de la page à propos

--- src/pages/parametres/a-propos.vue
+++ src/pages/parametres/a-propos.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-
+import IconCross from '@/components/icons/IconCross.vue';
 </script>
 <template>
 
 
     <main class="px-5 flex flex-col gap-12 py-12 mt-20">
-        <h1>A PROPOS</h1>
+        <header class="flex items-center justify-between pb-3 border-b border-slate-500">
+            <IconCross />
+            <h1 class="text-stone-100 text-xl text-center">
+                À propos
+            </h1>
+            <div></div>
+        </header>
     </main>
 </template>
