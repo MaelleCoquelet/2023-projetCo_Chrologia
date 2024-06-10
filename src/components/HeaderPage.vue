3068bbbf82c542ad07381b36a6f125624a1b4a17
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-11 00:22:34 +0200

    modification du changement de couleur selon la route de l'icône settings

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -3,23 +3,18 @@
 import { RouterLink } from 'vue-router';
 import IconLogo from '@/components/icons/IconLogo.vue';
 import IconSettings from '@/components/icons/IconSettings.vue';
-import { ref, computed, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
-const settingActive = ref(false)
-
-function disableSetting() {
-  settingActive.value = false
-} 
 </script>
 
 <template>
   <header class="fixed z-10 w-full top-0 py-6 bg-slate-700">
     <nav class="flex justify-between px-[20px] items-center">
-      <RouterLink to="/weekly" @click="disableSetting">
+      <RouterLink to="/weekly">
         <IconLogo />
       </RouterLink>
-      <RouterLink to="/parametres" @click="settingActive = !settingActive">
-        <IconSettings :class="{ 'icon-fillActive': settingActive }" />
+      <RouterLink to="/parametres">
+        <IconSettings :class="{ 'icon-fillActive': $route.path==='/parametres' }" />
       </RouterLink>
     </nav>
   </header>
