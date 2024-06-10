81eabe81b30134f79971af0203c9d9078daaeeb8
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-11 01:36:41 +0200

    corrections sur la page profil

--- src/pages/profil/index.vue
+++ src/pages/profil/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 import CardWeekly from '@/components/CardWeekly.vue';
+import Button from '@/components/Button.vue';
 </script>
 <template>
     <main class="flex flex-col py-12 mt-20 mb-20">
@@ -9,14 +10,14 @@ import CardWeekly from '@/components/CardWeekly.vue';
             <div class="flex justify-between px-5">
                 <img class="rounded-full h-20 relative top-[-40px]" src="/src/assets/img/palicoMHW.webp"
                     alt="Photo de profil">
-                <button class="bg-slate-900 text-white">éditer le profil</button>
+                <Button text="éditer le profil" variant="dark" class="w-fit h-fit translate-y-2" url="#"/>
             </div>
         </div>
         <div class="flex justify-evenly gap-3 px-5 pb-6">
-            <div class="flex flex-col flex-1 justify-center items-center rounded gap-1 p-2 bg-slate-900">
+            <RouterLink to="" class="flex flex-col flex-1 justify-center items-center rounded gap-1 p-2 bg-slate-900">
                 <p class="font-bold">Amis</p>
                 <p>3</p>
-            </div>
+            </RouterLink>
             <div class="flex flex-col flex-1 justify-center items-center rounded gap-1 p-2 bg-slate-900">
                 <p class="font-bold">Posts</p>
                 <p>4</p>
