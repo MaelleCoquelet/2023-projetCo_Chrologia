52db4e5bfb450b14775962bcf3f55f61c8e17b2b
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-10 14:40:03 +0200

    importation d'icônes

---
+++ src/components/icons/IconBlock.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_984_10821)">
+            <path
+                d="M12 2C6.475 2 2 6.475 2 12C2 17.525 6.475 22 12 22C17.525 22 22 17.525 22 12C22 6.475 17.525 2 12 2ZM4 12C4 7.58 7.58 4 12 4C13.85 4 15.545 4.635 16.9 5.685L5.685 16.9C4.635 15.545 4 13.85 4 12ZM12 20C10.15 20 8.455 19.365 7.1 18.315L18.315 7.1C19.365 8.455 20 10.15 20 12C20 16.42 16.42 20 12 20Z"
+                fill="#F5F5F4" />
+        </g>
+        <defs>
+            <clipPath id="clip0_984_10821">
+                <rect width="24" height="24" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>
+</template>
---
+++ src/components/icons/IconUnfollow.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M2 6C2 3.79086 3.79086 2 6 2H18C20.2091 2 22 3.79086 22 6V18C22 20.2091 20.2091 22 18 22H6C3.79086 22 2 20.2091 2 18V6Z"
+            stroke="#F5F5F4" stroke-linejoin="round" />
+        <path
+            d="M10 11C11.1046 11 12 10.1046 12 9C12 7.89543 11.1046 7 10 7C8.89543 7 8 7.89543 8 9C8 10.1046 8.89543 11 10 11Z"
+            stroke="#F5F5F4" stroke-linejoin="round" />
+        <path d="M10 13C7.79086 13 6 14.7909 6 17H14C14 14.7909 12.2091 13 10 13Z" stroke="#F5F5F4"
+            stroke-linejoin="round" />
+        <path d="M16 11.5H19" stroke="#F5F5F4" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
