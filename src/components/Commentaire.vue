2b7e4eb8663be6dd2e09b4da344d7dd6e1c0c526
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-10 23:01:46 +0200

    création du composant commentaire

---
+++ src/components/Commentaire.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="flex flex-col">
+        <div class="flex justify-between items-center">
+            <RouterLink to="#" class="flex gap-3 items-center">
+                <img class="rounded-full h-8 translate-y-2" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+                <p class="text-xs font-bold">Nom Utilisateur</p>
+            </RouterLink>
+            <p class="text-xs">DATE</p>
+        </div>
+        <p class="px-11 overflow-auto break-all">CONTENU COMMENTAIRE</p>
+    </div>
+</template>
