9a742117a156bdd17051d15aabe63986d59973a1
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-11 12:04:51 +0200

    ajout de la section liste d'amis pour la page profil

--- src/pages/profil/index.vue
+++ src/pages/profil/index.vue
@@ -2,6 +2,10 @@
 import { RouterLink } from 'vue-router';
 import CardWeekly from '@/components/CardWeekly.vue';
 import Button from '@/components/Button.vue';
+import IconCross from '@/components/icons/IconCross.vue';
+import AddPerson from '@/components/AddPerson.vue';
+import { ref } from 'vue';
+const overlayAmisOpen = ref(1)
 </script>
 <template>
     <main class="flex flex-col py-12 mt-20 mb-20">
@@ -10,14 +14,15 @@ import Button from '@/components/Button.vue';
             <div class="flex justify-between px-5">
                 <img class="rounded-full h-20 relative top-[-40px]" src="/src/assets/img/palicoMHW.webp"
                     alt="Photo de profil">
-                <Button text="éditer le profil" variant="dark" class="w-fit h-fit translate-y-2" url="#"/>
+                <Button text="éditer le profil" variant="dark" class="w-fit h-fit translate-y-2" url="#" />
             </div>
         </div>
         <div class="flex justify-evenly gap-3 px-5 pb-6">
-            <RouterLink to="" class="flex flex-col flex-1 justify-center items-center rounded gap-1 p-2 bg-slate-900">
+            <button @pointerdown="overlayAmisOpen = 1"
+                class="flex flex-col flex-1 justify-center items-center rounded gap-1 p-2 bg-slate-900">
                 <p class="font-bold">Amis</p>
                 <p>3</p>
-            </RouterLink>
+            </button>
             <div class="flex flex-col flex-1 justify-center items-center rounded gap-1 p-2 bg-slate-900">
                 <p class="font-bold">Posts</p>
                 <p>4</p>
@@ -34,5 +39,18 @@ import Button from '@/components/Button.vue';
             </div>
             <CardWeekly v-for="post in 5" />
         </div>
+        <section class="hidden px-5 flex-col gap-12 py-12 top-0 z-20 h-screen fixed w-full bg-slate-700"
+            :class="{ '!flex': overlayAmisOpen === 1 }">
+            <header class="flex items-center justify-between pb-3 border-b border-slate-500">
+                    <IconCross class="hover:cursor-pointer" @pointerdown="overlayAmisOpen = 0" />
+                <h1 class="text-stone-100 text-sm text-center">
+                    Liste d'ami(e)s
+                </h1>
+                <div></div>
+            </header>
+            <div class="flex flex-col gap-6">
+                <AddPerson v-for="notif in 7" />
+            </div>
+        </section>
     </main>
 </template>
