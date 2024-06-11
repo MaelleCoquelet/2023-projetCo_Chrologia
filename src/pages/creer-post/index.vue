2ae02d2e83354cd4708d98660d25c7e544f9d545
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-11 10:16:50 +0200

    mise en forme de la page creer post et correction de la structure html

--- src/pages/creer-post/index.vue
+++ src/pages/creer-post/index.vue
@@ -23,34 +23,36 @@ const postTypeSelected = ref(1)
             <div></div>
         </header>
         <section>
-            <div>
-                <img class="rounded-full h-16" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+            <div class="flex flex-col gap-6">
+                <div class="flex items-center justify-center">
+                    <img class="rounded-full h-16" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+                </div>
+                <h2 class="text-stone-100 text-sm font-normal">Créez votre reminder ou votre weekly !</h2>
+                <ul class="flex justify-around">
+                    <li>
+                        <button class="menuFeed-items" @pointerdown="postTypeSelected = 1">
+                            <IconWeekly :class="{ '*:fill-orpink-200 *:stroke-orpink-200': postTypeSelected === 1 }" />
+                            <p class="text-sm" :class="{ 'text-orpink-200 font-bold': postTypeSelected === 1 }">
+                                Weekly</p>
+                        </button>
+                    </li>
+                    <li>
+                        <button to="/reminder" class="menuFeed-items" @pointerdown="postTypeSelected = 2">
+                            <IconReminder :class="{ '*:fill-orpink-200': postTypeSelected === 2 }" />
+                            <p class="text-sm text-stone-100"
+                                :class="{ '!text-orpink-200 font-bold': postTypeSelected === 2 }">Reminder
+                            </p>
+                        </button>
+                    </li>
+                </ul>
             </div>
-            <h1>Créez votre reminder ou votre weekly !</h1>
-            <ul class="flex justify-around">
-                <li>
-                    <button class="menuFeed-items" @pointerdown="postTypeSelected = 1">
-                        <IconWeekly :class="{ '*:fill-orpink-200 *:stroke-orpink-200': postTypeSelected === 1 }" />
-                        <h1 class="text-sm" :class="{ 'text-orpink-200 font-bold': postTypeSelected === 1 }">
-                            Weekly</h1>
-                    </button>
-                </li>
-                <li>
-                    <button to="/reminder" class="menuFeed-items" @pointerdown="postTypeSelected = 2">
-                        <IconReminder :class="{ '*:fill-orpink-200': postTypeSelected === 2 }" />
-                        <h2 class="text-sm text-stone-100"
-                            :class="{ '!text-orpink-200 font-bold': postTypeSelected === 2 }">Reminder
-                        </h2>
-                    </button>
-                </li>
-            </ul>
-            <textarea class="text-sm text-stone-100 placeholder:text-stone-100 bg-transparent"
-                placeholder="Écrivez votre message ici"></textarea>
-            <div>
+            <textarea class="text-sm text-stone-100 placeholder:text-stone-100 bg-transparent w-full h-fit py-8"
+                placeholder="Écrivez votre post..."></textarea>
+            <div class="flex justify-between items-center">
                 <button>
                     <IconImage />
                 </button>
-                <Button />
+                <Button text="poster" url="/weekly" />
             </div>
         </section>
     </main>
