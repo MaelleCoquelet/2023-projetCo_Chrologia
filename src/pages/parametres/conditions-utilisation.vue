b907b303ad4572f2be0d3cbb4d956dd21f50c91b
AAmrouche <150058298+AAmrouche@users.noreply.github.com>
2024-06-11 11:11:04 +0200

    script + header

--- src/pages/parametres/conditions-utilisation.vue
+++ src/pages/parametres/conditions-utilisation.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import IconCross from '@/components/icons/IconCross.vue';
 
 </script>
 <template>
 
 
     <main class="px-5 flex flex-col gap-12 py-12">
-        <h1>CONDITIONS UTILISATIONS</h1>
+        <header class="flex items-center justify-between pb-3 border-b border-slate-500">
+            <RouterLink to="/parametres">
+                <IconCross />
+            </RouterLink>
+            <h1 class="text-stone-100 text-xl text-center">
+                Conditions d'utilisation
+            </h1>
+            <div></div>
+        </header>
+
     </main>
 </template>
