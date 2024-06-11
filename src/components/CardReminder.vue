af0d44115a76b01724abc004190e95d6dae0221e
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-11 14:10:22 +0200

    création du composant CardReminder

---
+++ src/components/CardReminder.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+import IconLike from '@/components/icons/IconLike.vue';
+import IconComment from '@/components/icons/IconComment.vue';
+import IconMore from '@/components/icons/IconMore.vue';
+import IconCross from './icons/IconCross.vue';
+import IconUnfollow from './icons/IconUnfollow.vue';
+import IconBlock from './icons/IconBlock.vue';
+import IconEye from './icons/IconEye.vue';
+import IconFlag from './icons/IconFlag.vue';
+import IconSend from './icons/IconSend.vue';
+import Commentaire from '@/components/Commentaire.vue';
+import { comment } from 'postcss';
+
+const activeText = ref(false)
+const overlayOpen = ref(0)
+</script>
+<template>
+    <article class="flex flex-col gap-6">
+        <div class="flex justify-between items-center">
+            <RouterLink to="/" class="flex gap-3 items-center">
+                <img class="rounded-full h-8" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+                <p>Nom Utilisateur</p>
+            </RouterLink>
+            <IconMore @pointerdown="overlayOpen = 1" />
+        </div>
+        <div class="flex flex-col gap-6">
+            <img src="" alt=" Photo Ici">
+            <div class="flex gap-1">
+                <p class="">Début de la description ici
+                    <span class="text-orpink-200 contents" @pointerdown="activeText = !activeText"
+                        :class="{ '!hidden': activeText }">...voir plus</span>
+                </p>
+                <p class="hidden" :class="{ '!contents': activeText }">
+                    fin de la description ici.
+                </p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <p>07<span>Jours</span></p>
+                <p>07<span>Heures</span></p>
+                <p>07<span>Minutes</span></p>
+            </div>
+        </div>
+        <ul class="flex justify-center gap-20">
+            <li class="flex gap-2 items-center">
+                <IconLike />
+                <p>12</p>
+            </li>
+            <li class="flex gap-2 items-center">
+                <IconComment @pointerdown="overlayOpen = 2" />
+                <p>4</p>
+            </li>
+        </ul>
+        <section
+            class="hidden py-11 rounded-t flex-col *:py-6 divide-y bg-slate-700 divide-slate-500 px-5 -translate-x-5 fixed z-20 bottom-0 w-full"
+            :class="{ '!flex': overlayOpen == 1 }">
+            <header class="flex items-center justify-between pb-3 border-b border-slate-500">
+                <IconCross @pointerdown="overlayOpen = 0" />
+                <h3 class="text-stone-100 text-xl text-center">
+                    Paramètres du post
+                </h3>
+                <div></div>
+            </header>
+            <div class="flex flex-col gap-6">
+                <h4 class="text-stone-100 text-sm text-bold">Infos du post</h4>
+                <div class="flex flex-row justify-center gap-6">
+                    <p class="flex flex-col justify-center items-center gap-1">Date de création<span>DATE</span></p>
+                    <p class="flex flex-col justify-center items-center gap-1">Date de publication<span>DATE</span></p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-6 justify-start items-start">
+                <button class="flex gap-6 items-center">
+                    <IconUnfollow />
+                    <p>Ne plus suivre l’utilisateur</p>
+                </button>
+                <button class="flex gap-6 items-center">
+                    <IconBlock />
+                    <p>Bloquer l’utilisateur</p>
+                </button>
+                <button class="flex gap-6 items-center">
+                    <IconEye />
+                    <p>Masquer le post</p>
+                </button>
+            </div>
+            <button class="flex gap-6 items-center">
+                <IconFlag />
+                <p>Signaler le post</p>
+            </button>
+        </section>
+
+        <section
+            class="hidden py-6 rounded-t flex-col gap-6 border-b border-slate-500 bg-slate-700 px-5 -translate-x-5 fixed z-30 bottom-0 w-full"
+            :class="{ '!flex': overlayOpen == 2 }">
+            <header class="flex items-center justify-between pb-3 border-b border-slate-500">
+                <IconCross @pointerdown="overlayOpen = 0" />
+                <h3 class="text-stone-100 text-xl text-center">
+                    Commentaires
+                </h3>
+                <div></div>
+            </header>
+            <div class="flex flex-col gap-6 max-h-screen overflow-scroll">
+                <Commentaire v-for="commentaire in 8" />
+            </div>
+            <div class="flex items-center justify-between gap-2">
+                <img class="rounded-full h-10" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
+                <input
+                    class="text-sm text-stone-100 placeholder:text-stone-100 in bg-slate-900 rounded-md px-3 py-[6px] overflow-auto break-all w-full"
+                    required placeholder="Saisir le message">
+                <button>
+                    <IconSend />
+                </button>
+            </div>
+        </section>
+    </article>
+</template>
