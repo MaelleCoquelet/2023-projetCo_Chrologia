8bb5f01805adf13d619037e2b04be2a2bc5f388e
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-06-13 13:44:47 +0200

    ajout des notification depuis le pocketbase

--- src/components/NotificationPerson.vue
+++ src/components/NotificationPerson.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-
+import { type UsersResponse } from '@/pocketbase-types';
+const props = defineProps<UsersResponse>()
 </script>
 <template>
     <div class="flex justify-between items-center">
-        <img class="rounded-full h-6 pr-2" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil">
-        <p class="overflow-auto break-all"><span>NOM_USER</span>a posté un
-            <span>TYPE_POST</span></p>
-        <p class="pl-2"><span>DATE</span>j</p>
+        <ImgPb :record="props" :filename="photoProfil" :height="24" :width="24" class="rounded-full" />
+        <!-- <img class="rounded-full h-6 pr-2" src="/src/assets/img/palicoMHW.webp" alt="Photo de profil"> -->
+        <p class="overflow-auto break-all"><span>{{ username }}</span> a posté un
+            <span>weekly</span>
+        </p>
+        <p class="pl-2"><span>3</span>j</p>
     </div>
 </template>
--- src/pages/notifications/index.vue
+++ src/pages/notifications/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import NotificationPerson from '@/components/NotificationPerson.vue';
+import { allUtilisateurs } from '@/backend';
+
+const usersListe = await allUtilisateurs()
 </script>
 <template>
     <main class="px-5 flex flex-col gap-12 py-12 mt-20">
         <h1 class="text-sm">Dernières notifications</h1>
         <section class="flex flex-col gap-8">
-            <NotificationPerson v-for="notif in 7" />
+            <NotificationPerson v-for="notif in usersListe" v-bind="notif" :key="notif.id" />
         </section>
     </main>
 
